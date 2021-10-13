67efcffff0078cb06da91163a7ce73d5af4aa02b
Ricardo <rikardo_kz@live.com.pt>
2021-10-13 22:24:42 +0100

    Add messages model

---
+++ Model/Messages.js
@@ -0,0 +1,30 @@
+const motivationMessages = [
+    "Be brave enough to start a conversation that matters.",
+    "Focus on the good.",
+    "Be your own kind of beautiful.",
+    "You don't always need a plan. Sometimes you just need to breathe. Trust. Let go. And see what happens.",
+    "I can correct your grammar, but only you can fix yourself.",
+    "The best is yet to come.",
+    "Always be thankful for everything you have.",
+    "Good things are going to happen if you only just smile.",
+    "Trust your soul, your body will follow.",
+    "Your are doing better than you think.",
+    "Don't just be good to others, be good to yourself.",
+
+];
+
+const hardMessages = [
+    "Unless you puke, faint, or die. KEEP GOING.",
+    "I don't broadcast every high and I don't hide every low. I'm triying to live. I'm not trying to convince the world I have a life.",
+    "Don't be afraid of being different, be afraid of being the same as everyone else.",
+    "Sometimes is best not to give a f*ck.",
+    "Don't carry your mistakes around with you. Instead, place them under your feet and use them as stepping stones to rise above them",
+
+]
+
+const heartBreakMessage = [
+    "They say 'follow your heart', but if your heart is in a million pieces which piece do you follow?",
+    "Not friend, not enemies, just some strangers with the same memories",
+    "I have so much left to say to you, but you're not here...",
+    "You can't treat people like shit and then expect them to love you",
+]
