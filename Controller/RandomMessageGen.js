c317f5e1a9146babfea7005ab8e99c64efd823bf
Ricardo <rikardo_kz@live.com.pt>
2021-10-28 22:07:44 +0100

    functionality all created for onclick

--- Controller/RandomMessageGen.js
+++ Controller/RandomMessageGen.js
@@ -9,11 +9,22 @@ const gym = document.getElementById("gym");
 const heart = document.getElementById("heart");
 const message = document.getElementById("message");
 
-function calculateRandomMessage(length) {
+function RandomIndex(length) {
     return Math.floor(Math.random() * length);
-}
+};
 
 
 wisdom.onclick = () => {
-    message.innerHTML = "&ldquo;" + motivationMessages[calculateRandomMessage(motivationMessages.length)] + "&ldquo;";
-}
+    message.style.color = "darkblue";
+    message.innerHTML = "&ldquo;" + motivationMessages[RandomIndex(motivationMessages.length)] + "&ldquo;";
+};
+
+gym.onclick = () => {
+    message.style.color = "black";
+    message.innerHTML = "&ldquo;" + gymMessage[RandomIndex(gymMessage.length)] + "&ldquo;";
+};
+
+heart.onclick = () => {
+    message.style.color = "darkred";
+    message.innerHTML = "&ldquo;" + heartBreakMessage[RandomIndex(heartBreakMessage.length)] + "&ldquo;";
+};
