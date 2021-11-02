0a8152a0d1ba72849e3644afa3a7a210dbef0c3f
Ricardo <rikardo_kz@live.com.pt>
2021-11-02 20:21:11 +0000

    connection to MongoDB established

---
+++ Controller/connect.js
@@ -0,0 +1,41 @@
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://usertestes:12345@cluster0.yy9fl.mongodb.net/Cluster0?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const dbName = "cluster0";
+/*
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+*/
+/*
+Replace <password> with the password for the usertestes user. 
+Replace myFirstDatabase with the name of the database that connections will use by default. 
+Ensure any option params are URL encoded.
+*/
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+        const db = client.db(dbName);
+
+        // Use the collection "people"
+        const col = db.collection("messagesDB");
+        const find = await col.find({
+            heartBreakMessage
+        });
+        
+        find.forEach(val => console.log(val));
+
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
